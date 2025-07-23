6a3e544a1f3e118b393b22f210254a0f3a8105ec
Coiness <1512133221@qq.com>
2025-07-23 14:59:12 +0800

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {persistStore, persistReducer,persistCombineReducers} from 'redux-persist';
 import recipeReducer from './recipeSlice';
 import inventoryReducer from './inventorySlice';
+
+const persistConfig = {
+  key:'root',
+  storage: localStorage, // 使用 localStorage 进行持久化存储
+  whitelist: ['recipes', 'inventory'], // 只持久化 recipes 和 inventory
+}
 
 const store = configureStore({
   reducer: {
